b53f713f17a0ddf5af1d2387941efa802c5999dc
Rohit Kumar <Mr.developer2003@gmail.com>
2026-07-21 16:41:28 +0530

    Basic cmd for SQL

---
+++ _1.Databases/_1.basic-cmd-sql.sql
@@ -0,0 +1,15 @@
+
+CREATE DATABASE CLASSDB; -- Create a database using CREATE DATABASE commands
+
+SHOW DATABASES; -- List all the DB's in your MySql Server
+
+CREATE DATABASE IF NOT EXISTS CLASSDB; -- It will only create database if it doesn't exits
+
+DROP DATABASE CLASSDB; -- Deleting the database
+USE CLASSDB; -- select a DB to works
+
+SHOW TABLES; -- list all tables in the selected DB 
+
+CREATE DATABASE FBDB;
+
+USE FBDB; -- select the new database 
