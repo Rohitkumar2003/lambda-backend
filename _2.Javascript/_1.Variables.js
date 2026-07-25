7a8b144341f943729950d5daad071b827b1b171e
Rohit Kumar <Mr.developer2003@gmail.com>
2026-07-26 00:19:44 +0530

    Variable in Js

---
+++ _2.Javascript/_1.Variables.js
@@ -0,0 +1,16 @@
+
+// 1. Create variables using var
+var score = 10;
+var marks = 20;
+console.log(`score:${score} marks:${marks}`);
+
+
+// 2. Create variables using let
+let age = 24;
+let flag = 0;
+console.log(`Age:${age} Flag:${flag}`);
+
+// 3. Create variables using const
+const x = 100;
+const y = 70;
+console.log(x,y);
