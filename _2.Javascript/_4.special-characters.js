8cd56d7b6a3b37bce74a8f743b54e4c14b1c3af7
Rohit Kumar <Mr.developer2003@gmail.com>
2026-07-26 23:27:09 +0530

    Special characters in Js

---
+++ _2.Javascript/_4.special-characters.js
@@ -0,0 +1,9 @@
+
+
+// special characters \n -> new line characters 
+var sentence = "The new Apple Iphone \n has been launched"
+// console.log(sentence); // The new Apple Iphone 
+                      //  has been launched
+
+var sentence2 = "The new Apple Iphone \t has been launched"
+console.log(sentence2); //The new Apple Iphone     has been launched
